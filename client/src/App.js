7a0dafc234c6ba4d268ab0a8c548bea52b8af03f
Korkrane <baudoin.haas@gmail.com>
2022-12-15 22:13:42 +0100

    .

--- client/src/App.js
+++ client/src/App.js
@@ -1,26 +1,35 @@
-import './App.css';
-import { Routes, Route} from 'react-router-dom';
 import Home from './Components/Home';
 import Signin from './Components/Signin';
 import Signup from './Components/Signup';
 import Unknown from './Components/Unkown';
 import Profile from './Components/Profile';
-
-
+import { Route, Routes } from 'react-router-dom';
+import { useContext } from 'react';
+import {socket, SocketContext} from './context/socket';
 
 function App() {
   return (
     <>
-      <Routes>
-          <Route path='/home' element={<Home />} />
-          <Route exact path='/signin' element={<Signin />} />
-          <Route path='/signup' element={<Signup />} />
-          <Route path='/profile' element={<Profile />} />
-          <Route path='*' element={<Unknown />} />
-      </Routes>
+    <Routes>
+        <Route path='/home' element={<HomeWithContext />} />
+        <Route exact path='/signin' element={<Signin />} />
+        <Route path='/signup' element={<Signup />} />
+        <Route path='/profile' element={<ProfileWithContext />} />
+        <Route path='*' element={<Unknown />} />
+    </Routes>
+
     </>
   );
 }
 
+function HomeWithContext() {
+  const socket = useContext(SocketContext);
+  return <Home socket={socket} />;
+}
+
+function ProfileWithContext() {
+  const socket = useContext(SocketContext);
+  return <Profile socket={socket} />;
+}
 
 export default App;
