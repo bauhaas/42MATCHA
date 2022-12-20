fd8e0fa3900d411f6ca35f2beb095ea76e95e9f7
thervieu <thibault99hervieu@gmail.com>
2022-12-20 16:03:21 +0100

    feat(back): notifications follow workflow

--- server/src/services/notificationsService.js
+++ server/src/services/notificationsService.js
@@ -1,40 +1,93 @@
 import pool from '../config/db.js';
 import log from '../config/log.js';
 
-//TODO
-// import { DBgetAllNotifications, DBdeleteNotification } from '../utils/queryNotificationsUtils.js';
+const createNotification = async (sender_id, receiver_id, type) => {
+    const client = await pool.connect();
+    
+    const notif = await client.query(`
+    INSERT INTO notifications(sender_id, receiver_id, type, read)
+    VALUES($1, $2, $3, $4)
+    RETURNING id
+    `, [sender_id, receiver_id, type, false]);
+
+    client.release();
+    return notif.rows[0].id;
+}
+
+const handleVisitAndMessage = async (sender_id, receiver_id, type) => {
+    const client = await pool.connect();
+    const notif = await client.query(`
+        SELECT * FROM notifications
+        WHERE sender_id = $1
+        AND receiver_id = $2
+        AND type = $3
+    `, [sender_id, receiver_id, type]);
+    console.log(notif);
+    var id = 0;
+    if (notif.rowCount == 0) {
+        id = await createNotification(sender_id, receiver_id, type);
+    } else {
+        id = notif.rows[0].id
+        if (notif.read === true) {
+            await updateReadNotification(id)
+        }
+        await updateTimeNotification(id)
+    }
+
+    client.release();
+    return id;
+}
 
 
-function getKey(value) {
-    const socketmap = global.socketUser;
-    return [...socketmap].find(([key, val]) => val == value)[0]
+export const getNotification = async (sender_id, receiver_id, type) => {
+    const client = await pool.connect();
+    const notif = await client.query(`
+    SELECT * FROM notifications
+    WHERE sender_id = $1
+    AND receiver_id = $2
+    AND type = $3
+    `, [sender_id, receiver_id, type]);
+
+    client.release();
+    if (notif.rowCount > 0) {
+        return notif.rows[0]
+    }
+    return null;
 }
 
 // Insert a new notification into the database
-export const insertNotification = async (sender_id, user_id, type) => {
+export const insertNotification = async (sender_id, receiver_id, type) => {
     try {
-        const client = await pool.connect();
-        const id = await client.query(`
-        INSERT INTO notifications(sender_id, user_id, type, read)
-        VALUES($1, $2, $3, $4)
-        RETURNING id
-        `, [sender_id, user_id, type, false]);
+        var id = 0;
+        if (type === "visit" || type === "message") {
+            return handleVisitAndMessage(sender_id, receiver_id, type)
+        }
+        
+        if (type == "like") {
+            
+            const liked = await getNotification(receiver_id, sender_id, type);
+            if (liked === null) {
+                return await createNotification(sender_id, receiver_id, type);
+            }
 
-        //Alert the user concerned by the notif
-        log.info('[notifService]', 'Current maps of socket connected:', global.socketUser);
-        const socketid = getKey(user_id);
-        const result = await client.query(`
-            SELECT *
-            FROM notifications
-            WHERE user_id = $1
-            `, [user_id]);
-        const notifications = result.rows;
-        log.info('[notifService]', socketid, 'must get \'receiveNotifs\' event');
-        const io = global.io;
-        io.to(socketid).emit('receiveNotifs', notifications);
+            await deleteNotification(liked.id);
+            await createNotification(sender_id, receiver_id, "match");
+            return await createNotification(receiver_id, sender_id, "match");
+        }
 
+        const likeNotif = await getNotification(sender_id, receiver_id, "like");
+        console.log(likeNotif);
+        if (likeNotif) {
+            await deleteNotification(likeNotif.id);
+        }
 
-        client.release();
+        const match = await getNotification(receiver_id, sender_id, "match");
+        if (match) {
+            const reverse = await getNotification(sender_id, receiver_id, "match");
+            await deleteNotification(match.id);
+            await deleteNotification(reverse.id);
+        }
+
         return id;
     } catch (err) {
         log.error('[notifService]', err);
@@ -84,7 +137,6 @@ export const updateReadNotification = async (id) => {
         throw err;
     }
 };
-
 
 
 // Delete a user from the database
