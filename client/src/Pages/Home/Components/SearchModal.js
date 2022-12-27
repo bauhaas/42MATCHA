1a802d63a77d2787416e0826bf5fbcc5d7125101
Korkrane <baudoin.haas@gmail.com>
2022-12-27 13:39:27 +0100

    ADD-FRONT: modal component to filter userlist

---
+++ client/src/Pages/Home/Components/SearchModal.js
@@ -0,0 +1,17 @@
+
+
+
+
+const SearchModal = () => {
+
+
+    return (
+        <>
+            <div className='fixed bottom-0 right-16 border-red-400 border-2'>
+                <div className='text-white text-2xl '>Search modal</div>
+            </div>
+        </>
+    )
+}
+
+export default SearchModal;
