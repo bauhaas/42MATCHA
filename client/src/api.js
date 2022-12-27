766356a3d8090792f6e7ff3cc4316aa01885a10c
Korkrane <baudoin.haas@gmail.com>
2022-12-27 19:11:07 +0100

    ADD-FRONT: set display based on user relation

--- client/src/api.js
+++ client/src/api.js
@@ -23,6 +23,19 @@ export const likeUserById = async (sender_id, receiver_id) => {
         });
 }
 
+export const unlikeUserById = async (sender_id, receiver_id) => {
+    return axios.post('http://localhost:3001/notifications', {
+        sender_id: sender_id,
+        receiver_id: receiver_id,
+        type: 'unlike'
+    })
+        .then(response => response.data)
+        .catch(error => {
+            console.log(error);
+        });
+}
+
+
 
 export const getUserById = async (id) => {
     return axios.get(`http://localhost:3001/users/${id}`)
